5ca2dbb1d6fca2029189f6748c708455e2b28995
Joe Zoll <jpzoll@syr.edu>
2023-09-24 16:37:08 -0400

    Initial Commit

---
+++ MeditationTimer/MeditationTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationTimerApp.swift
+//  MeditationTimer
+//
+//  Created by Joe Zoll on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MeditationTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
